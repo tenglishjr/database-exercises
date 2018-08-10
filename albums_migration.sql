23d03dd9da99a43e9504582a86d04992211f476e
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-10 14:32:51 -0500

    Fix spacing

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,22 +4,33 @@
 --
 -- The first command your albums_migration.sql will need is:
 USE codeup_test_db;
+
+
 -- Write some SQL to drop a table named albums if it exists.
 DROP TABLE IF EXISTS albums;
+
+
 -- Create an albums table with the following columns:
 CREATE TABLE albums (
+
 -- id — auto incrementing unsigned integer primary key
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+
 -- artist — string for storing the recording artist name
   artist VARCHAR(255) NOT NULL,
+
 -- name — string for storing a record name
   record_name VARCHAR(255) NOT NULL,
+
 -- release_date — integer representing year record was released
   release_date INT(4) UNSIGNED NOT NULL,
+
 -- sales — floating point value for number of records sold (in millions)
   sales_in_millions FLOAT UNSIGNED NOT NULL,
+
 -- genre — string for storing the record's genre(s)
   genre VARCHAR(255),
+
   PRIMARY KEY(id)
 );
 
