971d942393eccedaa2c401747d76abc5e405147e
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-15 09:27:39 -0500

    Added question numbers

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,2 +1,16 @@
-select concat(emp_no, ' - ', last_name, ', ', first_name) as full_name, birth_date as DOB from employees
+use employees;
+
+-- 3.
+select concat(last_name, ' ', first_name) as 'full_name'
+from employees
+limit 10;
+
+-- 4.
+select concat(last_name, ' ', first_name) as 'full_name', birth_date as 'DOB'
+from employees
+limit 10;
+
+-- 5.
+select concat(emp_no, ' - ', last_name, ' ', first_name) as 'full_name', birth_date as 'DOB'
+from employees
 limit 10;
