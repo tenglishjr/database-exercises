0ed72faf3847eca425121c476503e2ae48d0295e
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-15 09:22:27 -0500

    Fixed queries

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,14 +1,14 @@
 select distinct title from titles;
 
 select first_name, last_name from employees
-where last_name like 'E%' and
-last_name like '%e'
+where last_name like 'e%e'
 group by first_name, last_name;
 
-select last_name, count(last_name) from employees
+select last_name, count(*) from employees
 where last_name like '%q%' and last_name not like '%qu%'
 group by last_name;
 
-select count(gender), gender from employees
+select count(*), gender from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
-group by gender;
+group by gender
+order by count(*) desc;
